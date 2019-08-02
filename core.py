f3466e9ffe5700d93fd333b6fade5944672d2437
nokje <reda-@live.nl>
2019-08-02 14:11:51 +0200

    Update core.py

--- core.py
+++ core.py
@@ -2,12 +2,33 @@
 #endpoints. This script is used to determining the current state of functioning.
 #This script is validated on the following FortiGate(VM / FortiOS) firmware versions: 6.0.5, 6.2.0, 6.2.1
 
+
+#TODO list
+# Validate -p value (port range)
+
 #Import the modules part of this script (these need to be installed on forehand via > pip install)
-import requests
-import time
-import json
-import os
+import requests, time, json, os, sys, argparse, socket
 from datetime import datetime
+
+#Check if the correct argument is added to the call.
+parser = argparse.ArgumentParser(description='This script is used to validate functionality of a FortiGate device. A sum of common controls and checks are done based on the available FortiGate API endpoints. It stores how the FortiGate functions. This script has been tested on the following FortiGate(VM / FortiOS) firmware versions: 6.0.5, 6.2.0, 6.2.1')
+parser.add_argument("IP_ADDR", help="Enter the IP address of the FortiGate firewall")
+parser.add_argument("-p", help="Range = 1-65535. To use a different portnumber for the FortiGate adminportal. (default = 443)")
+args = parser.parse_args()
+
+#Check if the given IP address is a valid IPv4 address.
+try:
+    socket.inet_aton(args.IP_ADDR)
+except socket.error:
+    print('HealthCheck [%s]: The IP address is not a valid IPv4 address.' %run_id)
+    exit(1)
+
+#Check if the -p option contains a valid port number.
+#if args.p in range(1,65535):
+#   print( " %s is in the range"%str(n))
+#else :
+#   print("The number is outside the given range.")
+#   exit(1)
 
 #Suppres anoying InsecureRequestWarning
 requests.packages.urllib3.disable_warnings()
@@ -15,67 +36,33 @@ requests.packages.urllib3.disable_warnings()
 #Import secrets
 from secrets import *
 
-#Get run time for identifying this session in the fortmat used is yyyymmdd-h00m00
-now = datetime.now().strftime('%Y%m%d-h%Hm%M')
+#Get run time for identifying this session. The session identifier is 'yyyymmdd-h00m00' format.
+run_id = datetime.now().strftime('%Y%m%d-h%Hm%M')
+print('HealthCheck [%s]: Starting HealthCheck session.' %run_id)
 
 #Create folder structure for this itteration.
-print('HealthCheck: Building folder tree structure in scipt directory: data/%s' %now )
 path = os.path.dirname(os.path.realpath(__file__))
-data = '/'.join ([ path, 'data' ])
-datanow = '/'.join ([ path, 'data',now ])
-if os.path.isdir(datanow) == False:
-    os.makedirs(datanow)
+data_run = '/'.join ([ path, 'data',run_id ])
+if os.path.isdir(data_run) == False:
+    os.makedirs(data_run)
 
-#Functies welke gebruikt worden
-def check_con():
-    if r_sys.status_code == 200:
-        print('HealthCheck: Retrieved data')
-    elif r_sys.status_code != 200:
-        print('HealthCheck [ERROR]: Could not retrieve data')
+#Function to cURL the data
+def req_data( api, file_name ):
+    if args.p == None:
+        req_url = ''.join(['https://', args.IP_ADDR, api, auth])
+    else:
+        req_url = ''.join(['https://', args.IP_ADDR,':', args.p, api, auth])
+    try:
+        query = requests.get ( req_url, verify = False )
+    except requests.ConnectionError, e:
+        print(e), exit(1)
+    sys.stdout = open( '/'.join([data_run, file_name]), 'w+' )
+    print(query.text)
     return;
 
-#Buidling blocks voor de get api endpoints
-base_url = 'https://192.168.2.254:4443'
-api_mon_int = '/api/v2/monitor/system/available-interfaces/select'
-api_mon_rou = '/api/v2/monitor/router/ipv4/select'
-api_mon_sys = '/api/v2/monitor/system/status/select'
+print('HealthCheck [%s]: Acquired the data and stored it in my ./data folder.' %run_id )
+req_data( api = '/api/v2/monitor/system/available-interfaces/select', file_name = 'interface.txt' )
+req_data( api = '/api/v2/monitor/router/ipv4/select', file_name = 'routes.txt' )
+req_data( api = '/api/v2/monitor/system/status/select', file_name = 'system.txt' )
 
-
-#Bouw de urls op met de juiste API endpoints (vergeet de auth niet)
-    #url_int is used for retrieving the available interface status and configurations
-url_int = ''.join([base_url, api_mon_int, auth])
-    #url_route is used for retrieving the IPv4 routing table information
-url_route = ''.join([base_url, api_mon_rou, auth])
-    #url_sys is used to retrieve system status information
-url_sys = ''.join([base_url, api_mon_sys, auth])
-
-#Retrieve data from FortiGate API endpoints
-#export PYTHONWARNINGS = "ignore:Unverified HTTPS request"
-#def int_req():
-print('HealthCheck: Looking up system status')
-r_sys = requests.get (url_sys, verify=False)
-check_con()
-#    return;
-
-#int_req()
-exit()
-
-print('HealthCheck: Looking up interface data')
-r_int = requests.get (url_int, verify=False)
-check_con()
-
-print('HealthCheck: Looking up routing table data')
-r_rou = requests.get (url_route, verify=False)
-check_con()
-
-#Writing data to file archive
-time.sleep(2)
-f = open( '%s/system.txt' %datanow, 'w+' )
-f.write(r_sys.text)
-f.close
-f = open( '%s/interface.txt' %datanow, 'w+' )
-f.write(r_int.text)
-f.close
-f = open( '%s/routes.txt' %datanow, 'w+' )
-f.write(r_rou.text)
-f.close
+print('HealthCheck [%s]: Script finished the run.' %run_id )
